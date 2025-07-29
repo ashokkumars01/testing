a01aba59993564275653cf44c7a758e1708057bd
Ashok Kumar Shivashankar (MS/ETX1) <iash1kor@bosch.com>
2025-07-29 15:56:10 +0530

    Update2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
 
-variables = ["ENV_VAR1", "ENV_VAR2"]
+variables = ["ARTIFACTORY_INTERNAL_TOKEN", "ARTIFACTORY_TOKEN"]
 for var in variables:
     print(f"{var}: {os.getenv(var, 'Not Found')}")
